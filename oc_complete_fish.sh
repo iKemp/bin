9af2b88ca840269a840bb480518341f6ceb714df
Ingo Kempf <KmpI@viessmann.com>
2018-07-16 10:16:34 +0200

    very early oc fish completion

---
+++ oc_complete_fish.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#
+# very early work in progress
+#
+# custom fish completion for openshift origin cli 
+# source oc bash completion and transform into fish completion
+#
+# refs
+# https://fishshell.com/docs/current/index.html#completion
+# https://fishshell.com/docs/current/commands.html#complete
+# /usr/share/fish/completions/git.fish
+#
+
+echo create oc fish completion
+
+# read in bash completion and write to file
+oc completion bash > yolo.tmp
+
+# source bash completion file to execute commands later
+source ./yolo.tmp
+
+# remove tmp file
+rm yolo.tmp
+
+# entrypoint
+_oc
+#echo ${commands[*]}
+#echo ${commands[@]}    # geht beides
+
+outputfile_name=outputfile.tmp
+
+for i in ${commands[@]}; do
+    completion="complete -f -c oc -a $i"
+    echo $completion >> outputfile_name
+
+    # TODO complete for all subcommands
+done
+
+# copy outputfile to fish completion folder
+cp outputfile_name /home/kmpi/.config/fish/completions/oc.fish
+rm outputfile_name
+
